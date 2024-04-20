d7472f079306f300157052b81d6d4e4cdf86edf3
gammatheta <shyam02patel@gmail.com>
2024-04-20 16:57:34 -0400

    setup nn file

--- classification/neuralnetwork.py
+++ classification/neuralnetwork.py
@@ -1,7 +1,26 @@
 import util
 import math
 import numpy as np
+import time
+import random
+import string
+
+def activation_function():
+    pass
+
 class NeuralNetworkClassifier:
-    def __init__(self):
+    def __init__(self, legalLabels, max_iterations):
+        self.labels = legalLabels
+        self.max_iterations = max_iterations
+        self.weights = {}
+    
+
+    def setWeights(self, weights):
+        assert len(weights) == len(self.legalLabels)
+        self.weights = weights
+
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
         pass
 
+    def classify(self, data):
+        pass
